b21c1392aff804819683e14ef60158c08d18d043
DevMarvin <mmwanzi76@gmail.com>
2021-01-04 22:37:15 +0300

    face detect

---
+++ image_face_detect.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+image = cv2.imread('')
+
+while True:
+    grayimg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(greyimg, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+        rect_color = grayimg[y:y+h, x:x+w]
+
+    cv2.imshow('Detected', image)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
