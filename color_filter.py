c7c8a9cc6d71dac7f02bf5ab738ee5d4b58ad91d
DevMarvin <mmwanzi76@gmail.com>
2021-01-05 22:44:58 +0300

    blue filter

--- color_filter.py
+++ color_filter.py
@@ -8,13 +8,14 @@ while True:
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([50, 150, 30])
-    upper_red = np.array([180, 255, 255])
+    lower_blue = np.array([50, 150, 30])
+    upper_blue = np.array([180, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    #Mask out other colors to reamin with blue
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('res',res)
+    cv2.imshow('filtered',filtered)
     cv2.imshow('frame',frame)
 
     k = cv2.waitKey(5) & 0xFF
